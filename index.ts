c8ceeb4519ccc4db385c3a0a5ed7e56713305055
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-11-16 12:37:30 -0800

    StreamingFast endpoint okay

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {
+  authIssue,
   createRegistry,
-  createRequest,
-  parseAuthorization,
+  createRequest
 } from "@substreams/core";
 import { readPackageFromFile } from "@substreams/manifest";
 import { BlockEmitter, createDefaultTransport } from "@substreams/node";
@@ -13,9 +13,10 @@ dotenv.config();
 
 const substreamsEndpoint = process.env.SUBSTREAMS_ENDPOINT;
 invariant(substreamsEndpoint, "SUBSTREAMS_ENDPOINT is required");
-const substreamsApiToken = process.env.SUBSTREAMS_API_TOKEN;
-invariant(substreamsApiToken, "SUBSTREAMS_API_TOKEN is required");
+const substreamsApiKey = process.env.SUBSTREAMS_API_KEY;
+invariant(substreamsApiKey, "SUBSTREAMS_API_KEY is required");
 const authIssueUrl = process.env.AUTH_ISSUE_URL;
+console.log("AUTH_ISSUE_URL:", authIssueUrl);
 invariant(authIssueUrl, "AUTH_ISSUE_URL is required");
 
 // Configure logging with TSLog
@@ -23,32 +24,33 @@ logger.enable();
 
 // Download Substream package
 const manifest = "./geo-substream.spkg";
+console.log("Reading package from file:", manifest);
 const substreamPackage = await readPackageFromFile(manifest);
 
-const token = await parseAuthorization(
-  substreamsApiToken,
-  authIssueUrl
-);
+console.log("Parsing authorization...");
+const { token } = await authIssue(substreamsApiKey, authIssueUrl);
+console.log(token);
 const outputModule = "geo_out";
 const startBlockNum = 36472424;
 const productionMode = true;
 const finalBlocksOnly = false; // Set to true to only process blocks that have pass finality
 
-const headers = new Headers();
-headers.set("User-Agent", "geo-sink");
+
 
 // Cursor
 // const cursor = cursorPath.startsWith("http") ? httpCursor : fileCursor;
 
 // Connect Transport
 // const startCursor = await cursor.readCursor(cursorPath, httpCursorAuth);
+console.log("Creating registry...");
 const registry = createRegistry(substreamPackage);
+console.log("Creating transport...");
 const transport = createDefaultTransport(
   substreamsEndpoint,
   token,
   registry,
-  headers
 );
+console.log("Creating request...");
 const request = createRequest({
   substreamPackage,
   outputModule,
@@ -59,14 +61,17 @@ const request = createRequest({
 });
 
 // Substreams Block Emitter
+console.log("Creating block emitter...");
 const emitter = new BlockEmitter(transport, request, registry);
 
 // Stream Blocks
 emitter.on("anyMessage", (message, cursor, clock) => {
-  console.log(message);
-  console.log(cursor);
-  console.log(clock);
+  console.log("Received message:", message);
+  console.log("Cursor:", cursor);
+  console.log("Clock:", clock);
 });
 
 // Start streaming
+console.log("Starting emitter...");
 await emitter.start();
+console.log("Emitter started.");
